1ea2c0b972041890ff66d2c16ebd882bc6d6f2cd
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-10-09 23:56:06 +0200

    Create Calculate BMI.java

---
+++ 8kyu/Calculate BMI/Calculate BMI.java
@@ -0,0 +1,23 @@
+public class Calculate {
+  public static String bmi(double weight, double height) {
+    double bmi = weight / (height * height);
+    
+    if (bmi <= 18.5) {
+      return "Underweight";
+    }
+    
+    if (bmi <= 25.0) {
+      return "Normal";
+    }
+      
+    if (bmi <= 30.0) {
+      return "Overweight";
+    }
+    
+    if (bmi > 30) {
+      return "Obese";
+    }
+    
+    return null;
+  }
+}
