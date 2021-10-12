d3f0f0146c35cde355fc149e5030381703fe2542
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-10-12 21:44:08 +0200

    Create Sum Arrays.java

---
+++ 8kyu/Sum Arrays/Sum Arrays.java
@@ -0,0 +1,11 @@
+public class SumArray {
+  public static double sum(double[] numbers) {
+    double result = 0;
+    
+    for (double number : numbers) {
+      result += number;
+    }
+    
+    return result;
+  }
+}
