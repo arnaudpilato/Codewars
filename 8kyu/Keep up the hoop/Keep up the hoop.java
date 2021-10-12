261bf32e357b9e8acd4ffc0d945cc0d5fb4c0734
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-10-12 21:39:42 +0200

    Create Keep up the hoop.java

---
+++ 8kyu/Keep up the hoop/Keep up the hoop.java
@@ -0,0 +1,7 @@
+public class HelpAlex{
+  public static String hoopCount(int n){
+    String message = (n >= 10) ?  "Great, now move on to tricks" : "Keep at it until you get it";
+    
+    return message;
+  }
+}
