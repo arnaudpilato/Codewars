9bead88c423bcfda46d95a45702afedf838bef3b
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-10-13 16:34:14 +0200

    Create Opposites Attract.java

---
+++ 8kyu/Opposites Attract/Opposites Attract.java
@@ -0,0 +1,7 @@
+public class OppositesAttract {
+  public static boolean isLove(final int flower1, final int flower2) {
+    boolean love = (flower1 % 2 == 0 && flower2 % 2 == 1 || flower1 % 2 == 1 && flower2 % 2 == 0) ? true : false;
+    
+    return love;
+  }
+}
