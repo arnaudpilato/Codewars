d789e17ff371516e89dfc632fd2907fb24be98d8
Pil <70975556+arnaudpilato@users.noreply.github.com>
2021-10-17 00:28:08 +0200

    Update Insane Coloured Triangles.java

--- 2kyu/Insane Coloured Triangles/Insane Coloured Triangles.java
+++ 2kyu/Insane Coloured Triangles/Insane Coloured Triangles.java
@@ -1,59 +1,21 @@
-import java.util.*;
-
 public class Kata {
-  private static final int[][] arr = {{1,0,0},{1,1,0},{1,2,1}};
-  
   public static char triangle(final String row) {
-    int n = row.length()-1;
-    String n3 = Integer.toString(n,3);
-    CharCounter counter = new CharCounter();
+    char[] arr = row.toCharArray();
     
-    for (int m = 0; m <= n; m++) {
-      int prod = 1;
-      String m3 = Integer.toString(m,3);
+    for (int length = arr.length; length > 1; ) {
+      int n = 1;
       
-      for(int i = 0; i < m3.length(); i++){
-        int mdigit = Character.getNumericValue(m3.charAt(m3.length()-i-1));
-        int ndigit = Character.getNumericValue(n3.charAt(n3.length()-i-1));
-        prod *= arr[ndigit][mdigit];
+      for (; 3 * n < length; n *= 3) {
         
-        if (prod == 0) break;
       }
       
-      if (prod != 0)
-        counter.add(row.charAt(m), prod % 3);
+      for (int i = 0; i < length - n; i++) {
+        arr[i] = (char) ((3 - ((arr[i] + arr[i + n]) % 3)) % 3);
+      }
+      
+      length -= n;
     }
     
-    return counter.getUnique();
+    return arr[0] == 1 ? 'R' : arr[0] == 2 ? 'G' : 'B';
   }
 }
-
-class CharCounter {
-  private Map<Character,Integer> chars; 
-  {
-    chars = new HashMap<>();
-    chars.put('R',0);
-    chars.put('G',0);
-    chars.put('B',0);
-  }
-  
-  public void add (char ch, int n){
-    chars.put(ch, chars.get(ch) + n);
-  }
-  
-  public char getUnique(){
-    int uniqueCount = 0;
-    
-    for (char ch: chars.keySet()){
-      int simplifiedCount = chars.get(ch) % 3;
-      uniqueCount ^= simplifiedCount;
-      chars.put(ch,simplifiedCount);
-    }
-    
-    for (char ch: chars.keySet()){
-      if (chars.get(ch) == uniqueCount) return ch;
-    }
-    
-    return '0';
-  }
-} 
